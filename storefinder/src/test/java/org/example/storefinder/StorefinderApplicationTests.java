33f984228afde8d229c1cdc396a51a85d9b1987d
Rohith Vishnumolakala <vrohith@cisco.com>
2021-08-07 16:05:55 +0530

    Add basic project files for java

---
+++ storefinder/src/test/java/org/example/storefinder/StorefinderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.storefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorefinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
